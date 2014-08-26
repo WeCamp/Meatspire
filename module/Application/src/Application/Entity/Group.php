66bf6513f40b7ae0026af50f27581853df3d00bc
Rick Kuipers <rick93@gmail.com>
2014-08-26 17:03:53 +0200

    Add get/set for group entity

--- module/Application/src/Application/Entity/Group.php
+++ module/Application/src/Application/Entity/Group.php
@@ -33,6 +33,102 @@ class Group
     protected $description;
 
     /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param \Application\Entity\User $organizer
+     */
+    public function setOrganizer($organizer)
+    {
+        $this->organizer = $organizer;
+    }
+
+    /**
+     * @return \Application\Entity\User
+     */
+    public function getOrganizer()
+    {
+        return $this->organizer;
+    }
+
+    /**
      * @ORM\Column(name="image", type="string", nullable=true)
      * @var string
      */

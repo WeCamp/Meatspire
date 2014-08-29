629ea70c507f92a3f61a7f8c91152c007d93e484
Rick Kuipers <rick93@gmail.com>
2014-08-29 07:55:29 +0200

    Add check if tokenfield exists

--- public/js/meetspire.js
+++ public/js/meetspire.js
@@ -7,31 +7,33 @@ function extractLast( term ) {
 }
 
 $( document ).ready(function() {
-	var groups = $('.tokenfield').attr('data-groups').split(',');
-	$('.tokenfield').tokenfield({
-		autocomplete: {
-			source: function( request, response ) {
-				// delegate back to autocomplete, but extract the last term
-				response( $.ui.autocomplete.filter(
-					groups, extractLast( request.term )));
-			},
-			focus: function() {
-				// prevent value inserted on focus
-				return false;
-			},
-			select: function( event, ui ) {
-				var terms = split( this.value );
-				// remove the current input
-				terms.pop();
-				// add the selected item
-				terms.push( ui.item.value );
-				// add placeholder to get the comma-and-space at the end
-				terms.push( "" );
-				this.value = terms.join( ", " );
-				return false;
-			},
-			delay: 100
-		},
-		showAutocompleteOnFocus: true
-	});
+    if ($('.tokenfield').length) {
+        var groups = $('.tokenfield').attr('data-groups').split(',');
+        $('.tokenfield').tokenfield({
+            autocomplete: {
+                source: function( request, response ) {
+                    // delegate back to autocomplete, but extract the last term
+                    response( $.ui.autocomplete.filter(
+                        groups, extractLast( request.term )));
+                },
+                focus: function() {
+                    // prevent value inserted on focus
+                    return false;
+                },
+                select: function( event, ui ) {
+                    var terms = split( this.value );
+                    // remove the current input
+                    terms.pop();
+                    // add the selected item
+                    terms.push( ui.item.value );
+                    // add placeholder to get the comma-and-space at the end
+                    terms.push( "" );
+                    this.value = terms.join( ", " );
+                    return false;
+                },
+                delay: 100
+            },
+            showAutocompleteOnFocus: true
+        });
+    }
 });
